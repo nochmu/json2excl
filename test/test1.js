a5bca062700f23ad57b4454e1cde7490cd0e3ef0
nochmu <christian@noctaru.de>
2018-06-20 00:55:04 +0200

    Update test1.js

--- test/test1.js
+++ test/test1.js
@@ -1,9 +1,7 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+describe('fail', function() {
+    it('should fail', function() {
+      assert.fail();
   });
 });
 
